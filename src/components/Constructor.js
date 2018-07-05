65dcb14497d5c015c61eaee8facdd68725fcfb18
Sergei Vatagin <s.vatagin@gmail.com>
2018-07-05 20:18:37 +0300

    begin groupby fn

--- src/components/Constructor.js
+++ src/components/Constructor.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Grid, Input, Select } from 'semantic-ui-react';
+import { Button, Grid, Input } from 'semantic-ui-react';
 
 const ingredientsOptions = [
     { key: '1', value: 'Сыр', text: 'Сыр' },
@@ -13,34 +13,36 @@ export default class Constructor extends React.Component {
         this.setState({ currentAddInputText: '' });
         this.props.getIngredients(0);
     }
+
+    groupByType(arr) {
+        const arrO = [...new Set(arr.map(o => o.b))].map(key => ({ [key]: arr.filter(o => o.b == key) }))
+    }
+
     render() {
         return (
             <Grid columns={2} centered>
                 <Grid columns={3} centered>
                     <Button color="red">
                         Тесто
-            </Button>
+                    </Button>
                     <Button color="orange">
                         Мясо
-            </Button>
+                    </Button>
                     <Button color="olive">
                         Сыр
-            </Button>
+                    </Button>
                     <Button color="green">
                         Соус
-            </Button>
+                    </Button>
                 </Grid>
-                <Grid columns={2} centered>
+                <Grid columns={1} centered>
                     <Grid.Row stretched>
                         <Grid.Column>
                             <Input placeholder="Наименование..." />
                         </Grid.Column>
-                        <Grid.Column>
-                            <Select placeholder="Ингредиенты" options={ingredientsOptions} />
-                        </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </Grid>
-        )
-    };
+        );
+    }
 };
