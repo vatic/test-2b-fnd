ba8cfa96739ceb2f874d4c1ec44dcaa736c6c50d
Sergei Vatagin <s.vatagin@gmail.com>
2018-07-06 11:48:41 +0300

    add current pizza state

--- src/components/Constructor.js
+++ src/components/Constructor.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Grid, Input, Segment } from 'semantic-ui-react';
-
-const ingredientsOptions = [
-  { key: '1', value: 'Сыр', text: 'Сыр' },
-  { key: '2', value: 'Тесто слоёное', text: 'Тесто слоёное' },
-  { key: '3', value: 'Соус', text: 'Соус' },
-];
+import {
+ Button, Card, Grid, Input, Segment 
+} from 'semantic-ui-react';
 
 export default class Constructor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentTypeId: 1 };
+    this.state = {
+      currentTypeId: 1,
+      currentPizza: { name: '', ingredients: [] },
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
   }
 
   componentWillMount() {
@@ -21,12 +21,23 @@ export default class Constructor extends React.Component {
   }
 
   groupByKey(arr, key) {
-    const arrO = [...new Set(arr.map(o => o[key]))].map(k => ({ [k]: arr.filter(o => o[key] == k) }));
+    const arrO = [...new Set(arr.map(o => o[key]))].map(k => ({ [k]: arr.filter(o => o[key] === k) }));
     return arrO.reduce((acc, curr, index, self) => Object.assign(acc, { [Object.keys(curr)[0]]: curr[Object.keys(curr)[0]] }), {});
   }
 
   handleTypeChange(typeId) {
-    this.setState({ currentTypeId: typeId})
+    this.setState({ currentTypeId: typeId });
+  }
+
+  handleNameChange(event) {
+    // console.dir(event.target.value);
+    this.setState({ currentPizza: { name: event.target.value, ingredients: this.state.currentPizza.ingredients } });
+  }
+
+  handleIngredientClick(ingredient) {
+    const current = this.state.currentPizza;
+    console.log(current);
+    this.setState({ currentPizza: { name: current.name, ingredients: [...current.ingredients, ingredient] } });
   }
 
   renderTypes(types) {
@@ -44,9 +55,16 @@ export default class Constructor extends React.Component {
 
   renderIngredients(ingredients) {
     return ingredients.map(ingt => (
-      <Segment>{ingt.name}</Segment>
+      <Segment
+        key={ingt.id}
+        className="Constructor-ingredient"
+        onClick={() => this.handleIngredientClick(ingt)}
+      >
+        {ingt.name}
+      </Segment>
     ));
   }
+
 
   render() {
     const types = this.props.typesList.result;
@@ -54,21 +72,31 @@ export default class Constructor extends React.Component {
     let selectedIngredients = [];
     if (types && types.length > 0) {
       selectedIngredients = ingredients[this.state.currentTypeId] || [];
-      console.log(selectedIngredients);
     }
     return (
-      <Segment>
+      <Segment className="Constructor-wrapper">
         <Grid centered>
           <Grid.Row>
             <Grid.Column width={6}>
-              <Segment>
-                {this.renderTypes(types)}
-              </Segment>
-              {this.renderIngredients(selectedIngredients)}
-              <Segment.Group />
+              <Segment.Group>
+                <Segment>
+                  {this.renderTypes(types)}
+                </Segment>
+                {this.renderIngredients(selectedIngredients)}
+              </Segment.Group>
             </Grid.Column>
             <Grid.Column width={10}>
-              <Input placeholder="Наименование..." />
+              <Input
+                fluid
+                placeholder="Наименование..."
+                value={this.state.currentPizza.name}
+                onChange={this.handleNameChange}
+              />
+              <Card
+                header={this.state.currentPizza.name}
+                description={`Состав: ${this.state.currentPizza.ingredients.map(i => i.name).join(', ')}`}
+                meta="320г."
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
